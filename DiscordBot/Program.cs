a52fcc30a1877456ca31156e8a665cdbbcf065ff
hcarver <hcarver@islander.tamucc.edu>
2022-03-31 15:20:45 -0500

    update

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -1,7 +1,7 @@
-﻿using System;
-using System.Threading.Tasks;
-using Discord;
+﻿using Discord;
 using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
 
 public class Program
 {
@@ -14,8 +14,9 @@ public class Program
 	{
 		_client = new DiscordSocketClient();
 		_client.Log += Log;
-		await _client.LoginAsync(TokenType.Bot,
-			Environment.GetEnvironmentVariable("DiscordToken"));
+		var token = "https://discord.com/api/oauth2/authorize?client_id=959176639005200485&permissions=8&scope=bot";
+
+		await _client.LoginAsync(TokenType.Bot, token);
 		await _client.StartAsync();
 
 		// Block this task until the program is closed.
@@ -24,6 +25,15 @@ public class Program
 	private Task Log(LogMessage msg)
 	{
 		Console.WriteLine(msg.ToString());
+		return Task.CompletedTask;
+	}
+
+	private static Task ClientOnMessageReceived(SocketMessage arg)
+	{
+		if (arg.Content.StartsWith("!helloworld"))
+		{
+			arg.Channel.SendMessageAsync($"User '{arg.Author.Username}' successfully ran helloworld!");
+		}
 		return Task.CompletedTask;
 	}
 }
